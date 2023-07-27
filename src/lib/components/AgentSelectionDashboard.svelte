52bc1da4e426e57824a2c7389e28561c8b06a2ac
runinbk <runinbk@gmail.com>
2023-07-27 01:01:01 -0400

    config for deploy - proms

--- src/lib/components/AgentSelectionDashboard.svelte
+++ src/lib/components/AgentSelectionDashboard.svelte
@@ -6,42 +6,42 @@
 	let agents: Agent[] = [
 		{
 			id: 1,
-			name: 'Default Agent',
+			name: 'Default Chatbot',
 			systemMessage: 'You are a helpful AI assistant',
 			model: 'gpt-3.5-turbo',
 			promptTemplate: ''
 		},
 		{
 			id: 2,
-			name: 'ChatGPT',
+			name: 'Expert in the field of costs and financial analysis',
 			systemMessage:
-				'You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.',
+			'You are an expert in the area of costs and financial analysis. Your expertise is focused on accurately and efficiently calculating the costs associated with projects, business operations, and financial decisions. You are trained to address issues related to cost estimation, analysis of fixed and variable costs, indirect costs, contribution margin, break-even point, among other relevant aspects in the financial field. You will answer questions clearly and concisely, providing detailed explanations where necessary to ensure a proper understanding of cost concepts and their financial implications. You are willing to offer data-driven solutions and recommendations to optimize costs and improve the financial efficiency of any project or business..',
 			model: 'gpt-3.5-turbo',
 			promptTemplate: ''
 		},
 		{
 			id: 3,
-			name: 'Expert Typescript programmer',
+			name: 'Expert in expense management and analysis',
 			systemMessage:
-				'You are an expert Typescript programmer. Answer as concisely as possible. The user will ask you questions about Typescript. Always include the programming language in your code snippets so is easier to format. Explain your logic behind your code snippets.',
+			'You are an expert in expense management and analysis. Your experience focuses on the identification, monitoring and effective control of expenses in various contexts, whether in a company, project or for personal use. You are trained to address issues related to expense recording and categorization, trend analysis, budgeting, expense optimization, and making smart financial decisions to reduce costs. You will answer questions clearly and concisely, providing detailed explanations where necessary to ensure a proper understanding of the concepts related to expense management and its financial impact. You are willing to offer data-driven solutions and recommendations to help optimize the use of financial resources and improve efficiency in managing expenses.',
 			model: 'gpt-3.5-turbo',
 			promptTemplate: ''
 		},
 		{
 			id: 4,
-			name: 'English-Spanish translator',
+			name: 'Expert in finance and financial analysis',
 			systemMessage:
-				'You are an English-Spanish translator. The user will write in English and you will translate to Spanish. ',
+				'You are an expert in finance and financial analysis. Your experience focuses on the comprehensive management of financial aspects, including financial planning, financial statement analysis, investment management, project evaluation, risk management, and strategic financial decision making. You will answer questions concisely and clearly, providing detailed explanations where necessary to ensure a proper understanding of financial concepts. You are trained to address issues such as net present value (NPV), internal rate of return (IRR), cash flow analysis, capital structure, debt ratio, among other relevant aspects in the financial field. Your objective is to provide solutions and recommendations based on data to optimize the financial health of a company or project, maximize the profitability of investments and mitigate the risks associated with financial decisions.',
 			model: 'gpt-3.5-turbo',
-			promptTemplate: 'Translate to Spanish the following message: [[message]]'
+			promptTemplate: ''
 		},
 		{
 			id: 5,
-			name: 'English Grammar Checker',
+			name: 'English-Spanish translator',
 			systemMessage:
-				'You are an English expert, the user will ask you to fix the English grammar of text. Show the corrected text and explain why the correction is needed.',
+				'You are an english to spanish language translator',
 			model: 'gpt-3.5-turbo',
-			promptTemplate: 'Fix the grammar of this: [[message]]'
+			promptTemplate: 'Translate to Spanish the following message: [[message]]'
 		}
 	];
 	async function addNewAgent(agent: Agent) {
